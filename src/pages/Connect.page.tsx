04756d3d8abc6ab16140eea62e308c1e7b629d34
Sanjay Govind <sanjay.govind9@gmail.com>
2026-02-19 17:15:46 +1300

    test build

--- src/pages/Connect.page.tsx
+++ src/pages/Connect.page.tsx
@@ -1,8 +1,8 @@
 import { IconExclamationCircle } from '@tabler/icons-react';
+import { useTranslation } from 'react-i18next';
 import { Alert, Button, Space, Text } from '@mantine/core';
 import { Layout } from '@/components/Layout/Layout';
 import { useConfigStore } from '@/components/SettingsContext/SettingsContext';
-import { useTranslation } from 'react-i18next';
 
 export function ConnectPage() {
   const connect = useConfigStore((state) => state.connect);
@@ -12,6 +12,23 @@ export function ConnectPage() {
   return (
     <>
       <Layout>
+        <Alert
+          variant="light"
+          color="red"
+          title="Santroller 2 is not finished"
+          icon={<IconExclamationCircle />}
+        >
+          Santroller 2 is currently not finished. Right now, several features are missing when
+          compared to the normal tool, and there are likley a lot of crashes and unfinished features
+          <Space h="md" />
+          Notable missing features:
+          <ul>
+            <li>USB Host</li>
+            <li>Bluetooth</li>
+            <li>Wii Extension Emulation</li>
+            <li>PS2 Emulation</li>
+          </ul>
+        </Alert>
         {!navigator.hid && (
           <Alert
             variant="light"
@@ -20,6 +37,8 @@ export function ConnectPage() {
             icon={<IconExclamationCircle />}
           >
             This browser is not supported as it does not support WebHID.
+            <Space h="md" />
+            You need to use a chromium based browser, firefox based browsers don't support WebHID.
           </Alert>
         )}
         {navigator.hid && connected && (
